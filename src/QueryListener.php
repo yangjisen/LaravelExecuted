d6cede218a0989673b6eacfbcf26e5ea3c597440
YangJiSen <381722452@qq.com>
2021-04-25 14:09:05 +0800

    Feat: 增加文件路径输出

--- src/QueryListener.php
+++ src/QueryListener.php
@@ -3,7 +3,6 @@
 
 namespace YangJiSen\LaravelExecuted;
 
-use DateTimeInterface;
 use Illuminate\Database\Events\QueryExecuted;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
@@ -15,28 +14,98 @@ class QueryListener
     /**
      * Handle the event.
      *
-     * @param  QueryExecuted  $event
+     * @param  \Illuminate\Database\Events\QueryExecuted  $event
      * @return void
      */
     public function handle(QueryExecuted $event)
     {
         if (config('database.debug', false)) {
-            $bindings = $this->prepareBindings($event->bindings);
-            $queryStr = Str::replaceArray('?', $bindings, $event->sql);
-            Log::channel('sql')->info("({$event->time})\t$queryStr");
+            $time = $event->time;
+
+            if ($caller = $this->getCallerFromStackTrace()) {
+                Log::channel('sql')->info("{$caller['file']}\t{$caller['line']}");
+                Log::channel('sql')->info("({$this->transformTime($time)})\t" . $this->replaceBindings($event));
+            }
         }
     }
 
     /**
-     * @param array $bindings
+     * Replace the placeholders with the actual bindings.
+     *
+     * @param  \Illuminate\Database\Events\QueryExecuted  $event
+     * @return string
+     */
+    public function replaceBindings($event)
+    {
+        $sql = $event->sql;
+
+        foreach ($this->formatBindings($event) as $key => $binding) {
+            $regex = is_numeric($key)
+                ? "/\?(?=(?:[^'\\\']*'[^'\\\']*')*[^'\\\']*$)/"
+                : "/:{$key}(?=(?:[^'\\\']*'[^'\\\']*')*[^'\\\']*$)/";
+
+            if ($binding === null) {
+                $binding = 'null';
+            } elseif (! is_int($binding) && ! is_float($binding)) {
+                $binding = $event->connection->getPdo()->quote($binding);
+            }
+
+            $sql = preg_replace($regex, $binding, $sql, 1);
+        }
+
+        return $sql;
+    }
+
+    /**
+     * Format the given bindings to strings.
+     *
+     * @param  \Illuminate\Database\Events\QueryExecuted  $event
      * @return array
      */
-    public function prepareBindings(array $bindings)
+    protected function formatBindings($event)
     {
-        return collect($bindings)->map(function ($item) {
-            return ($item instanceof DateTimeInterface)
-                ? "'{$item->format('Y-m-d H:i:s')}'"
-                : "'{$item}'";
-        })->toArray();
+        return $event->connection->prepareBindings($event->bindings);
     }
+
+    /**
+     * Find the first frame in the stack trace outside of Telescope/Laravel.
+     *
+     * @param  string|array  $forgetLines
+     * @return array
+     */
+    protected function getCallerFromStackTrace($forgetLines = 0)
+    {
+        $trace = collect(debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS))->forget($forgetLines);
+
+        return $trace->first(function ($frame) {
+            if (! isset($frame['file'])) {
+                return false;
+            }
+            return ! Str::contains($frame['file'],
+                base_path('vendor'.DIRECTORY_SEPARATOR.$this->ignoredVendorPath())
+            );
+        });
+    }
+
+    /**
+     * Choose the frame outside of either Telescope/Laravel or all packages.
+     *
+     * @return string|null
+     */
+    protected function ignoredVendorPath()
+    {
+        if (! ($this->options['ignore_packages'] ?? true)) {
+            return 'laravel';
+        }
+    }
+
+    protected function transformTime($time)
+    {
+        if($time > 1000) {
+            return number_format($time/1000, 2, '.', '').'s';
+        }
+
+        return number_format($time, 2, '.', '').'ms';
+    }
+
 }
